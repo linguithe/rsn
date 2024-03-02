d480bfe09a055ff770fb785f6ee1c7a9351aa742
linguithe <87516482+linguithe@users.noreply.github.com>
2024-03-02 19:57:47 +0800

    vue testing part 1

--- backend/src/main/java/com/rsn/backend/controller/RecipeController.java
+++ backend/src/main/java/com/rsn/backend/controller/RecipeController.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rsn.backend.model.Recipe;
@@ -13,6 +15,10 @@ import com.rsn.backend.repo.RecipeRepo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -32,6 +38,39 @@ public class RecipeController {
             recipeRepo.findByTitle(title).forEach(recipes::add);
         }
         return ResponseEntity.ok(recipes);
+    }
+
+    @GetMapping("/recipes/{id}")
+    public ResponseEntity<Recipe> getRecipeById(@PathVariable("id") long id) {
+        Recipe recipe = recipeRepo.findById(id).get();
+        return ResponseEntity.ok(recipe);
+    }
+
+    @GetMapping("recipes/{ingredient}")
+    public ResponseEntity<List<Recipe>> getRecipesByIngredient(@PathVariable("ingredient") String ingredient) {
+        List<Recipe> recipes = new ArrayList<Recipe>();
+        recipeRepo.findByIngredients(ingredient).forEach(recipes::add);
+        return ResponseEntity.ok(recipes);
+    }
+
+    @PostMapping("/recipes")
+    public ResponseEntity<Recipe> createRecipe(@RequestBody Recipe recipe) {
+        
+    }
+
+    @PutMapping("/recipes/{id}")
+    public ResponseEntity<Recipe> updateRecipe(@PathVariable("id") long id, @RequestBody Recipe recipe) {
+        
+    }
+
+    @DeleteMapping("/recipes")
+    public ResponseEntity<HttpStatus> deleteAllRecipes() {
+        
+    }
+
+    @DeleteMapping("/recipes/{id}")
+    public ResponseEntity<HttpStatus> deleteRecipe(@PathVariable("id") long id) {
+        
     }
        
 }
